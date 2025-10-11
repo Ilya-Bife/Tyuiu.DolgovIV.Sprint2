88901916dd339cb1e3d22eb6ae10f086c4b32f92
Tyuiu.DolgovIV@outlook.com <Ilya2017.dolgov@yandex.ru>
2025-10-11 11:31:39 +0500

    Task4

---
+++ Tyuiu.DolgovIV.Sprint2.Task4.V1.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.DolgovIV.Sprint2.Task4.V1.Lib;
+
+namespace Tyuiu.DolgovIV.Sprint2.Task4.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            double x = 1;
+            double y = 2;
+
+            DataService ds = new DataService();
+
+            Assert.AreEqual(ds.Calculate(x, y), 2);
+        }
+    }
+}
---
+++ Tyuiu.DolgovIV.Sprint2.Task4.V1/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.DolgovIV.Sprint2.Task4.V1.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #2 | Выполнил: Долгов И. В. | АСОиУб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #2                                                               *");
+        Console.WriteLine("* Тема: Тернарный оператор                                                *");
+        Console.WriteLine("* Задание #4                                                              *");
+        Console.WriteLine("* Вариант #1                                                              *");
+        Console.WriteLine("* Выполнил: Долгов Илья Витальевич | АСОиУб-25-1                          *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая вычисляет требуемое значение с              *");
+        Console.WriteLine("* использованием тернарного оператора, где пользователь вводит значение   *");
+        Console.WriteLine("* переменных х у с клавиатуры, если x < y, то (x + 3) / y иначе           *");
+        Console.WriteLine("* (x + 1) / (x - 2)                                                       *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int x = Convert.ToInt32(Console.ReadLine());
+        int y = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine(ds.Calculate(x,y));
+
+        Console.ReadKey();
+    }
+}
