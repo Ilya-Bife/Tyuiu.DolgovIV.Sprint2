b10e7d3f298455ddfddf553bbeb355895dc792c6
Tyuiu.DolgovIV@outlook.com <Ilya2017.dolgov@yandex.ru>
2025-10-11 11:09:01 +0500

    Task3

---
+++ Tyuiu.DolgovIV.Sprint2.Task3.V8.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.DolgovIV.Sprint2.Task3.V8.Lib;
+
+namespace Tyuiu.DolgovIV.Sprint2.Task3.V8.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            double x = 0;
+            DataService ds = new DataService();
+
+            Assert.AreEqual(ds.Calculate(x), 0.75);
+        }
+    }
+}
